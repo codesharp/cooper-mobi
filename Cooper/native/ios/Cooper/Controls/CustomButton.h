61700ecdaeaf75681812d0d88c4d0c620c80919b
sunleepy <sunleepy@gmail.com>
2012-08-03 10:47:57 +0800

    update1

--- Cooper/native/ios/Cooper/Controls/CustomButton.h
+++ Cooper/native/ios/Cooper/Controls/CustomButton.h
@@ -5,7 +5,7 @@
 //  Created by Ping Li on 12-7-25.
 //  Copyright (c) 2012年 codesharp. All rights reserved.
 //
-
+////
 @interface CustomButton : UIButton 
 
 - (id)initWithFrame:(CGRect)frame image:(UIImage*)image;
